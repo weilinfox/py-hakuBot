3db604a44a0d83557113cce6ec76064c320627b7
weilinfox <weilin.king@qq.com>
2021-01-27 22:24:13 +0800

    add test environment scripy

---
+++ test_environment.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+# 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 您可以在下面的链接找到该许可证.
+# https://github.com/weilinfox/py-hakuBot/blob/main/LICENSE
+
+import flask
+import time, json, importlib, threading, os, requests, logging, json, sys, csv, socket
+
+print('No errors.')
