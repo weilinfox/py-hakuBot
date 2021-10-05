7fefb6a903828a2ff3b292de906a14962888924d
Weilin Cai <weilin.king@qq.com>
2021-10-05 12:33:44 +0800

    check bot status

---
+++ check.py
@@ -0,0 +1,56 @@
+#!/bin/python3
+# 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 您可以在下面的链接找到该许可证.
+# https://github.com/weilinfox/py-hakuBot/blob/main/LICENSE
+
+import flask
+import time, json, importlib, threading, os, traceback, random
+import sys, getopt, os, requests, json
+
+HOST = '0.0.0.0'
+PORT = 8002
+THREAD = False
+PROCESS = 1
+FLASKDEBUG = False
+
+flaskApp = flask.Flask(__name__)
+
+configFile = os.path.normpath(os.path.dirname(os.path.abspath(__file__))) + '/files/config.json'
+print(f'Load config file: {configFile}')
+port = 0
+if os.path.exists(configFile):
+    with open(configFile, 'r') as conFile:
+        try:
+            configJson = json.loads(conFile.read()).get('server_config', {})
+        except:
+            print('Load hakuBot config file failed.')
+        else:
+            port = configJson.get('listen_port', 0)
+    if not port:
+        print('Get hakuBot listen port failed.')
+        exit(1)
+else:
+    print('Cannot find hakuBot config file.')
+    exit(1)
+print(f'Opration will be send to host: 127.0.0.1:{port}')
+
+def check(name):
+    resjson = dict()
+    try:
+        resjson = requests.get(url=f'http://127.0.0.1:{port}/STATUS', params={'name':name}, timeout=(1,2)).json()
+    except Exception as e:
+        return {'code':400, 'message':'Request failed.'}
+
+    return {'code': 200, 'message': resjson}
+
+# 事件触发
+@flaskApp.route('/STATUS', methods=['GET'])
+def statusMsg():
+    nm = flask.request.args.get('name', '')
+    res = flask.make_response(flask.jsonify(check(nm)))
+    res.headers['Access-Control-Allow-Origin'] = "*"
+    res.headers['Access-Control-Allow-Methods'] = 'GET'
+    return res
+
+# 运行flask
+if __name__ == "__main__":
+    flaskApp.run(host=HOST, port=PORT, debug=FLASKDEBUG, threaded=THREAD, processes=PROCESS)
